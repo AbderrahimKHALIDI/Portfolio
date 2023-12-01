95bd165eff1eebf18f760492e1807a50bc2c6f80
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-12-01 21:02:52 +0100

    fix bug

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none  hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Abderrahim-khalidi-1.pdf"
+          href="./Abderrahim-khalidi-1.pdf"
           download
         >
           Download CV{" "}
