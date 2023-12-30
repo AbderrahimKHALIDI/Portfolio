79681e783636af14639c1f2524e12abc3863efa3
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-12-30 18:06:37 +0100

    add modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect } from "react";
+
 import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
@@ -5,18 +8,23 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import Hotjar from "@hotjar/browser";
 
 export default function Home() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      Hotjar.init(3809371, 6);
+    }
+  }, []);
   return (
     <main className=" flex flex-col items-center px-4">
- 
-    <Intro/>
-    <SectionDivider/>
-    <About/>
-    <Projects/>
-    <Skills/>
-    <Experience/>
-    <Contact/>
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
-  )
+  );
 }
